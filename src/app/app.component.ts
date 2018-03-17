2f8d03adc7352c8dcffc182614d1e6af13c5a9f4
Daniel Mallon <dmallon@Daniels-MacBook-Pro.local>
2018-03-17 14:52:46 -0400

    Steady progress, moving component pieces around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
-  columns = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  times = new Array(37);
-
-  test(i, j) {
-    console.log(i + "," + j)
-  }
-
-  ngOnInit() {
-    var j = 6
-    var am_pm = "am"
-    for (var i = 0; i < this.times.length; i ++) {
-      this.times[i] = j + " " + am_pm
-      j += .5
-      if (j == 13) {
-        j = 1
-        am_pm = "pm"
-      }
-    }
-  }
+export class AppComponent {
 
 }
--- src/app/meeting-slot/meeting-slot.component.ts
+++ src/app/meeting-slot/meeting-slot.component.ts
@@ -9,6 +9,32 @@ import { MEETINGS } from '../mock-meetings';
 })
 export class MeetingSlotComponent implements OnInit {
 
+  title = 'app';
+  columns = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  times = new Array(37);
+  dictionary = {}
+
+  onSelect(i, j) {
+    console.log(i + "," + j)
+    this.selectedSlot = i + "," + j
+    
+  }
+
+  ngOnInit() {
+    console.log(this.dictionary)
+    var j = 6
+    var am_pm = "am"
+    for (var i = 0; i < this.times.length; i ++) {
+      this.times[i] = j + " " + am_pm
+      j += .5
+      if (j == 13) {
+        j = 1
+        am_pm = "pm"
+      }
+    }
+  }
+
+
   meeting: Meeting = {
     slot: "",
     title: "",
@@ -19,10 +45,8 @@ export class MeetingSlotComponent implements OnInit {
   meetings = MEETINGS;
 
   selectedMeeting: Meeting;
+  selectedSlot;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
