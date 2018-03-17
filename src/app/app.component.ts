fd78ebe2df9963c9271085a9f63c0ecb4a89668d
Daniel Mallon <dmallon@Daniels-MacBook-Pro.local>
2018-03-16 23:01:35 -0400

    Getting table going

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  columns = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  test() {
+    console.log("yo")
+  }
+
 }
