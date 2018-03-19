d5728f96377a43de503be999697b6ec0bba0aa52
Daniel Mallon <dmallon@Daniels-MacBook-Pro.local>
2018-03-18 21:32:17 -0400

    Added time and thought more about what I will do for front end

--- src/app/meeting-slot/meeting-slot.component.ts
+++ src/app/meeting-slot/meeting-slot.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meeting } from '../meeting';
-import { MEETINGS } from '../mock-meetings';
+//import { MEETINGS } from '../mock-meetings';
 
 @Component({
   selector: 'app-meeting-slot',
@@ -13,6 +13,7 @@ export class MeetingSlotComponent implements OnInit {
   columns = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   times = new Array(37);
   meetings = {}
+  realTime
 
   onSelect(i, j) {
     console.log(i + "," + j)
@@ -23,12 +24,15 @@ export class MeetingSlotComponent implements OnInit {
       this.meetings[this.selectedSlot] = new Meeting
       this.selectedMeeting = this.meetings[this.selectedSlot]
       this.selectedMeeting.slot = this.selectedSlot
+      this.selectedMeeting.time = i
+      this.selectedMeeting.week = this.columns[j]
     }
     console.log(this.meetings)
     
   }
 
   ngOnInit() {
+    this.getMainTime()
     console.log(this.meetings)
     var j = 6
     var am_pm = "am"
@@ -40,6 +44,30 @@ export class MeetingSlotComponent implements OnInit {
         am_pm = "pm"
       }
     }
+    console.log(this.getDaysInMonth(4, 2018))
+  }
+
+  getTime(i) {
+    var time = i/2 + 6
+    var am_pm = "am"
+
+    if (time == 12.5)
+      am_pm = "pm"
+
+    if (time >= 13) {
+      time -= 12
+      am_pm = "pm"
+    }
+
+    if (time % 1 != 0) {
+      var newTime = time.toString().split(".")[0]
+      newTime += ":30 " + am_pm
+    } else {
+      var newTime = time + ":00 " + am_pm
+    }
+      
+    return newTime
+      
   }
 
 
@@ -56,5 +84,31 @@ export class MeetingSlotComponent implements OnInit {
   selectedSlot;
 
   constructor() { }
+
+  getDaysInMonth(month, year) {
+    // Since no month has fewer than 28 days
+    var date = new Date(year, month, 1);
+    var days = [];
+    console.log('month', month, 'date.getMonth()', date.getMonth())
+    while (date.getMonth() === month) {
+       days.push(new Date(date));
+       date.setDate(date.getDate() + 1);
+    }
+    return days;
+  }
+
+  getMainTime() {
+    var currentdate = new Date(); 
+    var datetime = 
+                 currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + " "
+                + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                
+                
+    this.realTime = datetime
+  }
+  
 
 }
--- src/app/meeting.ts
+++ src/app/meeting.ts
@@ -1,5 +1,7 @@
 export class Meeting {
   slot: string;
+  time: Number;
+  week: string;
   title: string;
   description: string;
   participants: string[];
